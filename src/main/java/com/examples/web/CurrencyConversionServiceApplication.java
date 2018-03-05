221a41db3b263cd3b860a1cb89bc2c3bb6c50ace
Sandhya <sandhyaj224@gmail.com>
2018-03-05 12:57:46 -0500

    First Commit

---
+++ src/main/java/com/examples/web/CurrencyConversionServiceApplication.java
@@ -0,0 +1,24 @@
+package com.examples.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
+
+@EnableFeignClients("com.examples.web")
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+	
+	@Bean
+	public AlwaysSampler defaultSampler()
+	{
+		return new AlwaysSampler();
+	}
+}
---
+++ src/main/java/com/examples/web/CurrencyExchangeServiceProxy.java
@@ -0,0 +1,21 @@
+package com.examples.web;
+
+import java.net.HttpURLConnection;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+//@FeignClient(name="currency-exchange-service-1", url="localhost:8003")
+@RibbonClient(name="netflix-zuul-api-gateway-server")
+@FeignClient(name="currency-exchange-service-1")
+public interface CurrencyExchangeServiceProxy {
+	
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversionBean retrieveExchangeValue
+	       (@PathVariable("from") String from, @PathVariable ("to") String to);
+	
+	
+
+}
